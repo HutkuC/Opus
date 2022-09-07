be08341afe92631b7cad156d80a6a9e0685cf7f2
utku <halil.celik@boun.edu.tr>
2022-09-07 19:56:22 +0300

    Update queue.py

--- cogs/queue.py
+++ cogs/queue.py
@@ -20,9 +20,9 @@ class Queue(commands.Cog):
         if len(self.queue[ctx.guild.id]) == 0:
             await ctx.send('```The queue is empty.```')
             return
-        text = '```\n'
-        text += 'Current song: ' + self.queue[ctx.guild.id][0][0] + ' - ' + self.queue[ctx.guild.id][0][1] + '\n'
+        msg = '```\n'
+        msg += 'Current song: ' + self.queue[ctx.guild.id][0][0] + ' - ' + self.queue[ctx.guild.id][0][1] + '\n'
         for i in range(1, len(self.queue[ctx.guild.id])):
-            text += str(i) + ' - ' + self.queue[ctx.guild.id][i][0] + ' - ' + self.queue[ctx.guild.id][i][1] + '\n'
-        text += '```'
-        await ctx.send(text)
+            msg += str(i) + ' - ' + self.queue[ctx.guild.id][i][0] + ' - ' + self.queue[ctx.guild.id][i][1] + '\n'
+        msg += '```'
+        await ctx.send(msg)
