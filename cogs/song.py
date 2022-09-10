c0a255820d67a7cda6e20a13fdafcd6a2623b336
utku <halil.celik@boun.edu.tr>
2022-09-10 21:45:47 +0300

    Add song.py

---
+++ cogs/song.py
@@ -0,0 +1,10 @@
+
+class Song:
+
+    def __init__(self, title, url, duration, requester, search_term, thumbnail):
+        self.title = title
+        self.url = url
+        self.duration = duration
+        self.requester = requester
+        self.search_term = search_term
+        self.thumbnail = thumbnail
